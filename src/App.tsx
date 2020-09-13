01ce5e461289ad4843d6cb2094f3eb1e47dc75f8
Wander <wanders1995@hotmail.com>
2020-09-13 15:13:24 -0400

    Feat(Styles): Add base styles

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ const App: FC = ()=>{
     <Fragment>
       <Normalize />
       <GlobalStyle />
-      klk
     </Fragment>
   );
 }
---
+++ src/Assets/styles/base.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+
+export default {
+  button: styled.div`
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+  `,
+
+  iconButton:styled.div`
+  `
+
+
+}
