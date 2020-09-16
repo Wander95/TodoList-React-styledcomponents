688ccd09692bdcaf0c13ab7f9d029c43b3d08fa5
Wander95 <wanders1995@hotmail.com>
2020-09-16 17:01:11 -0400

    Feat(Main): Add dinamic array

--- src/Layouts/Main/Main.view.tsx
+++ src/Layouts/Main/Main.view.tsx
@@ -1,7 +1,11 @@
-import React,{ FC } from 'react';
+import React,{ FC, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Theme } from 'Assets/styles/theme'
+import * as Yup from 'yup'
+
 import { IconButton,CardItem,Card,ButtonGroup } from 'Components';
+
+import {Formik,Form} from 'formik';
 
 import { 
   Container, 
@@ -14,15 +18,51 @@ import {
 
 const MainView:FC = ()=> {
   
+  const List:Array<ICardItemList> = [
+    {
+      description:'huevos',
+      price:65
+    },
+    {
+      description:'pepino',
+      price:85
+    }
+  ] 
+  const [cardItemList, setCardItemList] = useState< Array<ICardItemList> >(List)
 
+
+  const inputSchema = Yup.object().shape({
+    description:Yup.string()
+      .min(1,'Short')
+  })
+
+
+  const handlePress = (event: React.KeyboardEvent<HTMLInputElement>)=>{
+    if (event.key === 'Enter'){
+      console.log('object :>> ');
+      setCardItemList([]);
+    }
+  }
+
+  interface ICardItemList {
+    description?:string,
+    price?:number
+  }
+
+  
   return (
     <ThemeProvider theme={Theme}>
       <Container>
         <Card>
           <Header>Todo Market</Header>
-          <CardItem />
-          <CardItem />
-          <CardItem />
+  
+          {cardItemList.map((cardItem,index)=>
+            <CardItem {...{
+              key:`card-item${index}`,
+              name:cardItem.description,
+              price:cardItem.price}
+            }/>
+          )}
         
           <SummaryContainer>
             <SummaryCount>03 Items</SummaryCount>
@@ -31,7 +71,22 @@ const MainView:FC = ()=> {
 
           <ButtonGroup />
           
-          <Input />
+          <Formik 
+            validationSchema={inputSchema}
+            onSubmit={()=>{}}
+            initialValues={{description:'',price:0}}>
+              {(props)=>(
+                <Form>
+                  <Input 
+                    onKeyPress={handlePress}
+                    name='description' 
+                    onChange={props.handleChange} 
+                    value={props.values.description}/>
+                  {/* <Input type='number' name='price' onChange={props.handleChange} value={props.values.price}/> */}
+                </Form>
+              )}
+          </Formik>
+        
           <IconButton onClick={()=>{}}> + </IconButton>
         </Card>
       </Container>
