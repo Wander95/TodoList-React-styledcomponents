618f1f1dff85a1ad754fbac45cbf94b504b56698
Wander95 <wanders1995@hotmail.com>
2020-09-24 09:03:08 -0400

    Feat(Controller/store): Add redux devtoolextension

--- src/Controllers/store.ts
+++ src/Controllers/store.ts
@@ -1,11 +1,17 @@
 import { createStore,combineReducers } from 'redux';
 import items from './Todo'
+import selectState from './App'
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 
 const reducers = combineReducers({
-  items
+  items,
+  selectState
 })
 
-const store = createStore(reducers)
+const store = createStore(reducers, composeWithDevTools( ));
+
 
 export default store
