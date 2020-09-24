9a7ce0bc1455054e025fde802ec90e0eda6ba235
Wander95 <wanders1995@hotmail.com>
2020-09-24 09:09:57 -0400

    Refactor(CardItem): Delete local state, Add redux state to change the active state

--- src/Components/CardItem/CardItem.view.tsx
+++ src/Components/CardItem/CardItem.view.tsx
@@ -1,39 +1,23 @@
-import React,{ FC,useState } from 'react'
-import { Item, status } from 'types'
+import React,{ FC } from 'react'
+import { CardItemProps } from 'types'
 import { Container,Name,Price } from './CardItem.styled'
-import { changeStatusToDeleted,changeStatusToActive } from 'Controllers/Todo/todo.actions';
+import { switchActive } from 'Controllers/Todo/todo.actions';
 import { useDispatch } from 'react-redux'
 
 export interface ICardItem {
   name?:string,
   price?:number
 }
-const CardItem:FC<Item> = (props)=> {
-  const { description,price,index } = props;
+const CardItem:FC<CardItemProps> = (props)=> {
+  const { description,price,active,id } = props;
   const dispatch = useDispatch();
 
-  const [touched, setTouched] = useState<boolean>(false);
-
   const handleClick = ()=>{
-    setTouched(!touched)
-
-    if(touched && index !== undefined){
-      dispatch(changeStatusToDeleted({
-        status:status.DELETED,
-        index:index
-      }))
-    }else{
-      index !== undefined &&
-      dispatch(changeStatusToActive({
-        status:status.DELETED,
-        index:index
-      }))
-    }
-    
+    dispatch(switchActive(id))
   }
 
   const styledProps = {
-    touched,
+    touched:!active,
     onClick:handleClick
   }
   return (
