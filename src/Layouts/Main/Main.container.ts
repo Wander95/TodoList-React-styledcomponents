f280900424cbdaad8c679fc88eab709bed47ba9f
Wander95 <wanders1995@hotmail.com>
2020-09-24 08:42:51 -0400

    Feat(Main.container): Add filter functionality

--- src/Layouts/Main/Main.container.ts
+++ src/Layouts/Main/Main.container.ts
@@ -1,12 +1,35 @@
 
 import { connect } from 'react-redux'
-import { IAppState } from 'types'
+import { IAppState,ButtonGroupSelection } from 'types'
 import Main from './Main.view'
 
+const selectFilteredItems = (state:IAppState)=>{
+  const { selectState } = state;
 
-const mapStateToProps = (state:IAppState={ items:[]}) => ({
-  items:state.items
-})
+  if(selectState.filterBy === ButtonGroupSelection.all){
+      return state.items
+  }
+
+  if(selectState.filterBy === ButtonGroupSelection.active){
+    return state.items.filter((item)=>{
+      return item.active
+    })
+  }
+
+  if(selectState.filterBy === ButtonGroupSelection.deleted){
+    return state.items.filter(item=>{
+      return !item.active
+    })
+  }
+
+  return state.items
+}
+
+const mapStateToProps = (state:IAppState) => {
+  return {
+    items:selectFilteredItems(state)
+  }
+}
 
 const mapDispatchToProps = {
   
