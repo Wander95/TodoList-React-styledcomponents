143b9bddf2d60fc5f9c1dc717cb6cae2670ea164
Wander95 <wanders1995@hotmail.com>
2020-09-27 19:49:43 -0400

    Feat(Modal): Add modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import React,{ Fragment, FC} from 'react';
 import { Normalize } from 'styled-normalize';
 import GlobalStyle from 'Assets/styles/global'
 import MainView from 'Layouts/Main';
+import { ModalProvider } from 'styled-react-modal'
 
 const App: FC = ()=>{
   return (
     <Fragment>
+      {/*  */}
       <Normalize />
       <GlobalStyle />
-      <MainView />
+
+      <ModalProvider>
+        <MainView />
+      </ModalProvider>
     </Fragment>
   );
 }
