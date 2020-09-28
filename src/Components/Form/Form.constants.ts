5b8782134f4712bd5528dc142797ef8c6e06bd0a
Wander95 <wanders1995@hotmail.com>
2020-09-27 21:36:28 -0400

    Add(MaxValue): MaxValue to input when entering numbers

--- src/Components/Form/Form.constants.ts
+++ src/Components/Form/Form.constants.ts
@@ -13,6 +13,7 @@ export const inputSchema = Yup.object().shape({
     .required('Is required'),
   price:Yup.number()
     .min(0,'Min value 1')
+    .max(99999,'max value 4 digits only')
     .required('Is required')
 })
 
